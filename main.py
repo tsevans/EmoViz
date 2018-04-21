090ddb11135cc72bb1b6a567828c92117c693d04
Tom Evans <tsevans@vt.edu>
2018-04-21 13:02:05 -0400

    removed test cases and cleaned up execution

--- main.py
+++ main.py
@@ -4,26 +4,8 @@ import platform
 
 
 if __name__ == '__main__':
-# Tests for radarcharts
-#     csv_list = [fr.find_file(1), fr.find_file(64), fr.find_file(65)]
-#     df_list = []
-#     for csv in csv_list:
-#         df_list.append(fr.csv_to_spark_dataframe(csv)[0])
-#     gsp.RadarChart.generate(df_list)
-
-# Tests for heatmaps
-    id = 1
-    hmp_test_csv = fr.find_file(id)
-    hmp_test_df, spark_session = fr.csv_to_spark_dataframe(hmp_test_csv)
-    gsp.HeatMap.generate(hmp_test_df, str(id))
-    spark_session.get_active_session().stop()
-
-# # Tests for ribbon plots
-#     id = 65
-#     rib_test_csv = fr.find_file(id)
-#     rib_test_df, spark_session = fr.csv_to_spark_dataframe(rib_test_csv)
-#     gsp.RibbonPlot.generate(rib_test_df, str(id))
-#     spark_session.get_active_session().stop()
+    pass
+    # run_job()
 
 
 def run_job(keys, viz_type):
