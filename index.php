87af913c8d666946b65b19c1f138109edf4dfcdd
Matt Barnett <mattb2@vt.edu>
2018-05-07 17:35:48 -0400

    Fixed exec call format, added plot type codes

--- index.php
+++ index.php
@@ -54,8 +54,10 @@
         
     </head>
     <body>
+    <!-- PHP Logic, Setting Variables, and Data Validation 
+         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     	<?php
-			$fileName = $plotType = $execCall = "";
+			$fileName = $plotType = $plotCode = $execCall = "";
 			$fileCount = 0;
 			$errorFlag = -1;
 			
@@ -67,7 +69,6 @@
 				if ($fileName != NULL) {
 					foreach($fileName as $value1) {
 						$fileCount++;
-						$execCall .= $value1 . ' ';
 					}
 				}
 				
@@ -80,7 +81,8 @@
 				} else {
 					if ($plotType == 'Radar') {
 						if ($fileCount <= 5) {
-							$execCall .= ' ' . $plotType;
+							$plotCode = 'RAD';
+							$execCall .= ' ' . $plotCode . ' ';
 							$errorFlag = 0;
 						} else {
 							$execCall = "You have selected too many data files for the Radar Visualization. Please select 1-5 data files and try again.";
@@ -88,20 +90,37 @@
 						}
 					} elseif ($plotType == 'Heat Map') {
 						if ($fileCount == 1) {
-							$execCall .= ' ' . $plotType;
+							$plotCode = 'HMP';
+							$execCall .= ' ' . $plotCode . ' ';
 							$errorFlag = 0;
 						} else {
 							$execCall = "You have selected too many data files for the Heat Map. Please select 1 data file and try again.";
 							$errorFlag = 1;
 						}
+					} elseif ($plotType == 'Line') {
+						if ($fileCount <= 2) {
+							$plotCode = 'LNG';
+							$execCall .= ' ' . $plotCode . ' ';
+							$errorFlag = 0;
+						} else {
+							$execCall = "You have selected too many data files for the Line Visualization. Please select 1-2 data files and try again.";
+							$errorFlag = 1;
+						}
 					} else {
 						if ($fileCount == 1) {
-							$execCall .= ' ' . $plotType;
+							$plotCode = 'RIB';
+							$execCall .= ' ' . $plotCode . ' ';
 							$errorFlag = 0;
 						} else {
 							$execCall = "You have selected too many data files for the Ribbon Visualization. Please select 1 data file and try again.";
 							$errorFlag = 1;
 						}
+					}
+				}
+				
+				if ($fileName != NULL && $errorFlag == 0) {
+					foreach($fileName as $value1) {
+						$execCall .= $value1 . ' ';
 					}
 				}
 				
@@ -128,7 +147,7 @@
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             <div class="row" style="height: 528px;">
             <div class="three columns" style="border-style: solid; padding: 8px; margin: 0 20px 0 10px; border-color: #484f59; border-radius: 2px;" >
-                    <!--<h3 class="section-heading">Data:</h3>-->
+            
                     <div id="data-content" style="overflow: auto; height: 75vh; width: 100%">
                     Select at least one data file below: <br /><br />
                         <?php
@@ -157,7 +176,7 @@
                       		}				
 
 						?>
-                        <!-- onclick="load_file(\'' . $value . '\')"  -->
+                        
                     </div>
                 </div>
                 <div class="nine columns" style="border-style: solid; padding: 8px; margin-left: 0px; border-color: #484f59; border-radius: 2px;" >
@@ -177,6 +196,7 @@
 								}
 							}
 							echo '<br />Plot type: ' . $plotType;
+							echo '<br />Call: ' . $send;
 							echo '</p>';
 							echo $doc->saveHTML();
 						} elseif ($errorFlag == 1) {
@@ -207,36 +227,20 @@
                 </div>
             </div>
                 <div class="graph-choice">
-                    <!--<h4 class="value-heading"><a href="#" onclick="reset_plots()">Line</a></h4>-->
                     <label class="control control--radio">Radar<input type="radio" name="plotType" <?php if (isset($plotType) && $plotType=="Radar") echo "checked";?> value="Radar" class="button-primary" style="margin: 0;"><span class="control__indicator"></span></label>
             	</div>
                 <div class="graph-choice">
-                    <!--<h4 class="value-heading"><a href="#" onclick="reset_plots()">Radar</a></h4>-->
                     <label class="control control--radio">Heat Map<input type="radio" name="plotType" <?php if (isset($plotType) && $plotType=="Heat Map") echo "checked";?> value="Heat Map" class="button-primary" style="margin: 0;"><span class="control__indicator"></span></label>
             	</div>
                 <div class="graph-choice">
-                    <!--<h4 class="value-heading"><a href="#" onclick="reset_plots()">Ribbon</a></h4>-->
                     <label class="control control--radio">Ribbon<input type="radio" name="plotType" <?php if (isset($plotType) && $plotType=="Ribbon") echo "checked";?> value="Ribbon" class="button-primary" style="margin: 0;"><span class="control__indicator"></span></label>
             	</div>
+                <div class="graph-choice">
+                    <label class="control control--radio">Line<input type="radio" name="plotType" <?php if (isset($plotType) && $plotType=="Line") echo "checked";?> value="Line" class="button-primary" style="margin: 0;"><span class="control__indicator"></span></label>
+            	</div>
                 <input type="submit" name="submit" value="Generate" class="generate" style="color: green; padding: 5px 5px 0 8px;">
-                <!--<div class="generate">
-                    <h4 class="value-heading"><a style="color: green;" href="#" onclick="load_plot()">Generate</a></h4>
-                    
-            	</div>-->
         </form>    
         </div>
-        <!--
-        <div class="container">
-            <div class="row">
-                <div class="twelve columns" style="border-style: solid;" >
-                    <h3 class="section-heading">Datasets will be selected from here.</h3>
-                    <p style="height: 100px;">
-                    	And here.
-                    </p>
-                </div>
-            </div>
-        </div>
-        -->
         
               <!-- End Document
             –––––––––––––––––––––––––––––––––––––––––––––––––– -->
